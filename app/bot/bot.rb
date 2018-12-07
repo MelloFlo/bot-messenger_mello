1958859ccea71ea458de491bd7db9f07009ab04d
MelloFlo <florian_pinto@outlook.fr>
2018-12-07 12:29:33 +0100

    add payload

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,11 +4,31 @@ include Facebook::Messenger
 
 Bot.on :message do |message|
 
-      if message.text.include? "Bonjour"
-        message.reply(text: "Salut p'tit gars")
-      elsif message.text.include? "Bye"
-        message.reply(text: "Bon vent")
-      else
-        message.reply(text: 'On dis bonjour avant tout, et aurevoir à la fin')
-      end
+
+
+
+message.reply(
+  text: "T'es prêt ?",
+  quick_replies: [
+    {
+      content_type: 'text',
+      title: "Ouiiiii !",
+      payload: 'HARMLESS'
+    },
+    {
+      content_type: 'text',
+      title: "Non !",
+      payload: 'ZERO'
+    }
+  ]
+)
+
+if message.payload == "HARMLESS"
+  message.reply(text: "T'es cool!")
+end
+if message.payload == "ZERO"
+  message.reply(text: "T'es pas cool!")
+end
+
+
 end
